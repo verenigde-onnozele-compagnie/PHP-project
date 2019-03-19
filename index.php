9d49153f19d229c018aecb41beafd38b347a5ab9
Kamiel Johan <kamielvandelande@gmail.com>
2019-03-19 16:09:12 +0100

    Banner design
    begonnen aan de design van de banner

--- index.php
+++ index.php
@@ -1,7 +1,42 @@
-<?php require 'header.php'; ?>
-
-   <h1>Welcome to AMO Login system Home Page! </h1>
-   <p>Nothing special to see here but that's not the point.</p>
-   <p>Try going to the ultra secret ultimate vip <a href='admin.php'> admin </a> page without logging in first</p>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Gokkers</title>
+</head>
+<body>
+<header>
+    <a href="#">Home</a>
+    <a href="login.php">Login</a>
+    <a href="register.php">Sign up</a>
+</header>
+<div class="banner">
+    <div class="banner-item">
+        <h2>What does it do?</h2>
+        <p>Jump in the shoes of Joe, Bob or Al and place bets in an intense and exiting dog race!</p>
+        <p>Will your chosen player be the one to reach a million euros?</p>
+    </div>
+    <div class="active">
+        <div class="banner-item">
+            <h2>Why you should download it!</h2>
+            <p>Bet on exiting and fast-paced dog races in this awesome app.</p>
+            <p>Designed and created by the master programmer Sem!</p>
+            <p>easy to use hard to master, this app guarantees hours of fun!</p>
+        </div>
+    </div>
+    <div class="banner-item">
+        <h2>How does is work?</h2>
+        <p>You begin with choosing either Joe, Bob or Al.</p>
+        <p>Then continue with choosing the amount of cash you want to bet.</p>
+        <p>Finally you end with placing the bet on your favorite dog and confirming your bet by pressing the "inzet" button.</p>
+        <p>Start the race with the "RACE!" button and watch the hounds battle in a race!</p>
+    </div>
+</div>
+</body>
+</html>
 
 <?php require 'footer.php'; ?>
